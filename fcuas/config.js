64a4b0a0661eca52d14b499de64020e80d235627
SkiF4er <cajeerskif4er@gmail.com>
2026-03-20 00:08:16 +0300

    Update config.js

--- fcuas/config.js
+++ fcuas/config.js
@@ -1,6 +1,6 @@
 export const APP_CONFIG = {
   supabaseUrl: 'https://lmsyycfecaqjyvkjcpbl.supabase.co',
   supabaseAnonKey: 'sb_publishable_rCuLpBPNLNYdhrDXFG6PJQ_In-G4SBZ',
-  appTitle: 'FCUAS',
+  appTitle: 'Таблица частот',
   defaultColor: '#D32F2F',
 };
